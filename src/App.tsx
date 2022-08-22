bc84cfc78e4d32b53f957028650786ade9d3bef3
Alberto <a.marzetta@yahoo.com>
2022-08-22 16:09:46 +0100

    push

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import {MaternityCalculator} from "./Forms/maternityCalculator";
+import {Box} from "@mui/material";
+function App() {
+  return (
+    <Box>
+      <MaternityCalculator />
+    </Box>
+  );
+}
+
+export default App;
